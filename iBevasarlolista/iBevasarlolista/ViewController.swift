2818c96fc6149b934e7ca57208f07823afa51088
martinhuszti <martinhuszti@gmail.com>
2019-10-15 13:06:42 +0200

    Created Single View App

---
+++ iBevasarlolista/iBevasarlolista/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBevasarlolista
+//
+//  Created by Martin on 2019. 10. 15..
+//  Copyright © 2019. Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
